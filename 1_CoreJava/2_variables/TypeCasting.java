42b2532c3caa92eb1ad130a0dce6e30282ad94c9
Sovan Roy <roysovan00@gmail.com>
2024-02-04 11:55:22 +0530

    TypeCasting added

---
+++ 1_CoreJava/2_variables/TypeCasting.java
@@ -0,0 +1,27 @@
+
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        //narrowing
+        byte b= 127;
+        System.out.println(b); //127
+        int a = 257;
+        b = (byte) a; // casting
+        System.out.println(b); // 257 % 256 = 1
+
+
+        float f= 5.6f;
+        int x = (int)f; // 5
+        System.out.println(x);
+
+
+
+
+        //type promotion (widening)
+        byte num1 = 10;
+        byte num2= 30;
+        int result = num1 * num2; // promoting type
+        System.out.println(result);
+
+    }
+}
