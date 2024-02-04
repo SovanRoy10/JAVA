2d75e8520515d32bfc0e1ecb631cbac4c64d71c0
Sovan Roy <roysovan00@gmail.com>
2024-02-04 12:39:21 +0530

    added LogicalOperator

---
+++ 1_CoreJava/2_variables/LogicalOperator.java
@@ -0,0 +1,16 @@
+
+
+public class LogicalOperator {
+    public static void main(String[] args) {
+    // and , or , not
+    // && , || , !
+        int x= 7;
+        int y= 5;
+        int a = 5;
+        int b= 9;
+
+
+        boolean result = x> y && a>b;
+        System.out.println(!result); //true
+    }
+}
