336a096e86edf0d5d97a019c8bc4ac10f4799a15
Sovan Roy <roysovan00@gmail.com>
2024-02-04 11:52:00 +0530

    Literal added

---
+++ 1_CoreJava/2_variables/Literal.java
@@ -0,0 +1,22 @@
+public class Literal {
+    public static void main(String[] args) {
+        //literals : All kind of initialized value is called literal
+
+
+        int num1 = 0b101; // binary number
+        int num2 = 0x7E; // hexadecimal number
+
+        int num3 = 10_00_00_00;
+
+        double num4 = 12e10;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+
+        char c = 'a';
+        System.out.println(++c);
+
+    }
+}
