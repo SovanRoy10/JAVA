82a0f6aa2870192c8d38361c3dffd044a9e48a7b
Sovan Roy <roysovan00@gmail.com>
2024-02-04 11:45:53 +0530

    initial commit

--- 1_CoreJava/2_variables/Variables.java
+++ 1_CoreJava/2_variables/Variables.java
@@ -1,5 +1,17 @@
 public class Variables {
     public static void main(String[] args) {
-        System.out.println("sovan");
+        int num1 = 5;
+		int num2 =3;
+		int result = num1 + num2;
+
+		short s = 10;
+        byte b = 14;
+        long lg= 12345l;
+        double db = 282.828; // default is double not float
+        float f = 123.90f;
+        char ch = 'k';
+        boolean bool = true;
+
+		System.out.println(bool);
     }
 }
