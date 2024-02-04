afc32aaf09ead07bfcbcbead8be65768f9e213e4
Sovan Roy <roysovan00@gmail.com>
2024-02-04 12:49:27 +0530

    added ifElse

---
+++ 1_CoreJava/3_conditional_statement/IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+    public static void main(String[] args) {
+
+        int x = 8;
+        int y =7;
+
+        if (x > y) {
+            System.out.println("Hello");
+        } else {
+            System.out.println("bye");
+        }
+
+    }
+}
