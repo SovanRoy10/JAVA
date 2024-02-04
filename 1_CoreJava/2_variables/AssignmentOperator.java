3775498ee5e38cf60bbcae64365eba9551e611fc
Sovan Roy <roysovan00@gmail.com>
2024-02-04 12:33:00 +0530

    added assignment and relational operator

---
+++ 1_CoreJava/2_variables/AssignmentOperator.java
@@ -0,0 +1,20 @@
+
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2= 5;
+
+
+        // int result = num1* num2; //35
+        // int result = num1- num2; //2
+        // int result = num1+ num2; //12
+        // int result = num1/ num2;// 1
+        int result = num1% num2; // 2
+        System.out.println(result);
+        num1+=2;
+        num1++; //post increment
+        --num1; //pre decrement
+        
+    }
+}
