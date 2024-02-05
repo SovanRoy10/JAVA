0f487a026f8811759953a03e929378a341fb89c8
Sovan Roy <roysovan00@gmail.com>
2024-02-05 18:02:01 +0530

    updated for loop and while loop

---
+++ 1_CoreJava/5_loops/DoWhile.java
@@ -0,0 +1,8 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 6;
+        do {
+            System.out.println("Hello " + i++);
+        } while (i <= 5);
+    }
+}
